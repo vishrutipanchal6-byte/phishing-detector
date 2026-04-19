5e8746fc5ec218f82f5a7ff5de73f934ea6be8a2
vishrutipanchal6-byte <vishrutipanchal6@gmail.com>
2026-04-19 14:04:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,17 @@ from datetime import datetime
 import sqlite3
 import re
 import os
-app = Flask(__name__)
-app.secret_key = "secret123"
+import pickle
+
+model_path = os.path.join(os.getcwd(), "model.pkl")
+model = pickle.load(open(model_path, "rb"))
 
 # 🟢 DATABASE CONNECTION
+import os
+
 def get_db():
-    conn = sqlite3.connect("database.db")
+    db_path = os.path.join(os.getcwd(), "database.db")
+    conn = sqlite3.connect(db_path)
     conn.row_factory = sqlite3.Row
     return conn
 
